bdfea8e844e1253a889b070c41873f4eb399a42f
Mathijs Riezebos <robijn.bird@gmail.com>
2017-03-24 15:00:10 +0100

    Refractoring correction
    -made exactly one mistake in my previous commit.

--- src/main/java/biomesoplenty/common/world/NoiseGeneratorBOP.java
+++ src/main/java/biomesoplenty/common/world/NoiseGeneratorBOP.java
@@ -81,7 +81,7 @@ public class NoiseGeneratorBOP extends NoiseGenerator
     
     public double wraparound(double d)
     {
-        long intD = MathHelper.floor(d);
+        long intD = MathHelper.lfloor(d);
         return d - intD + (intD % 0x1000000);        
     }
 
